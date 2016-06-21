36af2525e93dba2e5c93038597603d847d53d266
77dfeba <349841726@qq.com>
2016-06-21 20:53:53 +0800

    Initial commit

---
+++ app/src/main/java/cn/fuzhenyu/silntgym/Welcome.java
@@ -0,0 +1,25 @@
+package cn.fuzhenyu.silntgym;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+/**
+ * Created by 77dfeba on 2016/6/7.
+ */
+
+public class Welcome extends Activity {
+    public void onCreate(Bundle savedInstantState){
+        super.onCreate(savedInstantState);
+        setContentView(R.layout.welcome);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(Welcome.this,MainActivity.class);
+                startActivity(intent);
+                Welcome.this.finish();
+            }
+        },2000);
+    }
+}
